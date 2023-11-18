41d055ce0e49ff69a14a9236dc5490cd92142533
dmaberlin1 <dmaberlin77@gmail.com>
2023-11-18 12:34:48 +0200

    feat: -AspectJ Weaver dependency -LoggingAspect Class

--- src/main/java/com/dmadev/task7/aspect/LoggingAspect.java
+++ src/main/java/com/dmadev/task7/aspect/LoggingAspect.java
@@ -10,12 +10,21 @@ import org.aspectj.lang.annotation.Aspect;
 public class LoggingAspect {
 
     // Этот метод будет вызываться вокруг всех методов в пакете ru.itsinfo.repository
-    @Around("execution(* com.dmadev.task7.repositories.*.*(..))")
-    public Object aroundAllRepositoriesMethodsAdvice(ProceedingJoinPoint joinPoint) throws Throwable{
+    @Around("execution( *com.dmadev.task7.repositories.*.*(..))")
+    public Object aroundAllRepositoriesMethodsAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
         // Выводим в консоль сообщение о начале выполнения метода
         System.out.println("Start Method (joinPoint.getSignature()) = " + joinPoint.getSignature());
 
         //Засекаем время начала выполнения метода
         long start = System.currentTimeMillis();
+
+        //Вызываем метод вокруг которого оборачиваемся
+        Object proceeded = joinPoint.proceed();
+
+        // Выводим в консоль сообщение о завершении выполнения метода и времени, затраченном на выполнение
+        System.out.println(joinPoint.getSignature().getName() + " executed in" + (System.currentTimeMillis() - start) + "ms");
+
+        // Возвращаем результат выполнения метода
+        return proceeded;
     }
 }
